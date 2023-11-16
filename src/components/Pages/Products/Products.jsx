e821a9a0ce1698bc8773fa740668451087c2d7ad
Mashhura <mashhura2020@gmail.com>
2023-11-16 15:07:39 +0500

    categories added

--- src/components/Pages/Products/Products.jsx
+++ src/components/Pages/Products/Products.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import './Products.scss';
-import products from './products.json';
+import { useState,useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const Products = (props) => {
 
+    const [data, setData] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const category = searchParams.get('category');
+    
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then((res) => res.json())
+            .then((data) => {
+                if (!category) {
+                    setData(data)
+                } else {
+                    const filteredData = data.filter((item) => {
+                        return item.category == category
+                    })
+                    setData(filteredData);
+                }
+            })
+    }, [category])
+
     return (
+        <div>
+
+            <div className='categories'>
+                <button className='category-btn' onClick={() => { setSearchParams({ category: "men's clothing" }) }}>men's clothing</button>
+                <button className='category-btn' onClick={() => { setSearchParams({ category: 'jewelery' }) }}>jewelery</button>
+                <button className='category-btn'  onClick={() => { setSearchParams({ category: 'electronics' }) }}>electronics</button>
+                <button className='category-btn' onClick={() => { setSearchParams({ category: "women's clothing" }) }}>women's clothing</button>
+            </div>
+
         <main className='products container'>
             <div className='product-box'>
             {
-                products.map((product) => {
+                data.map((product) => {
                     return (
                        
                        <div className="product-card" key={product.id}>
@@ -24,7 +54,8 @@ const Products = (props) => {
                 })
                 }
                 </div>
-        </main>
+            </main>
+            </div>
     )
 }
 export default Products;
